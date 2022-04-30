13c20923e3d80877ec8b8ed1495e491e8513d1f3
tanyp <typ1805@qq.com>
2022-04-30 08:57:16 +0800

    更新

--- docs/sql/tansci_cloud.sql
+++ docs/sql/tansci_cloud.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 127.0.0.1
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50724
- Source Host           : 127.0.0.1:3306
+ Source Server Version : 50721
+ Source Host           : localhost:3306
  Source Schema         : tansci_cloud
 
  Target Server Type    : MySQL
- Target Server Version : 50724
+ Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 24/04/2022 15:03:46
+ Date: 30/04/2022 08:49:07
 */
 
 SET NAMES utf8mb4;
@@ -37,6 +37,10 @@ CREATE TABLE `log_error_info`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '异常日志' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Records of log_error_info
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for log_info
 -- ----------------------------
 DROP TABLE IF EXISTS `log_info`;
@@ -57,6 +61,11 @@ CREATE TABLE `log_info`  (
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '操作日志' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of log_info
+-- ----------------------------
+INSERT INTO `log_info` VALUES ('0df9dc36-b509-4c5d-89e6-d7b2d8012941', '用户信息-根据名称获取用户信息', 'SELECT', '根据名称获取用户信息', '{\"username\":\"admin\"}', '{\"code\":200,\"fail\":false,\"message\":\"操作成功\",\"result\":{\"address\":\"兰州安宁区永新华世界湾\",\"birthday\":\"2022-02-14\",\"createTime\":\"2022-04-29T15:22:20\",\"email\":\"admin@qq.com\",\"id\":\"bc3ac26e69731b617eb80274453f6dbb\",\"idCard\":\"\",\"nickname\":\"管理员\",\"password\":\"$2a$10$tlWWfjTObqLsC6ONrhNL/.GIpAoFu205TXPK6xUPHHr1kA/paK4lq\",\"phone\":\"18893817562\",\"sex\":0,\"status\":0,\"type\":1,\"updateTime\":\"2022-04-29T15:22:18\",\"username\":\"admin\"},\"success\":true}', 15, 'bc3ac26e69731b617eb80274453f6dbb', 'admin', 'com.tansci.controller.SysUserController.findByUsername', '/sysUser/findByUsername', '10.168.1.66', '1.0.0', '2022-04-29 16:00:31');
 
 -- ----------------------------
 -- Table structure for sys_dic
@@ -80,25 +89,75 @@ CREATE TABLE `sys_dic`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Records of sys_dic
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for sys_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu`  (
   `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键id',
-  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单编码',
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
-  `parent_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '父菜单ID',
-  `type` int(10) NULL DEFAULT NULL COMMENT '菜单类型',
-  `status` int(1) NULL DEFAULT NULL COMMENT '状态：0：删除，1：正常，2：禁用',
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单UR',
-  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
-  `level` int(10) NULL DEFAULT NULL COMMENT '级别 0：平台 1：其他',
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
+  `parent_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '父ID',
+  `chinese_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '中文名称',
+  `english_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '英文名称',
+  `status` int(1) NOT NULL COMMENT '状态：0：删除，1：正常，2：禁用',
+  `type` int(1) NOT NULL COMMENT '类型：0、菜单，1、按钮，2、链接',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'URL',
+  `icon` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
+  `level` int(10) NOT NULL COMMENT '级别',
   `sort` int(10) NULL DEFAULT NULL COMMENT '排序',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '菜单' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES ('09a724dfb41f47e50cb88ecd577c1655', 'index', '0', '首页', 'Index', 1, 0, NULL, 'HomeFilled', 1, 1, '2022-04-29 15:18:17', '2022-04-29 15:18:20', NULL);
+INSERT INTO `sys_menu` VALUES ('09a724dfb41f47e50cb88ecd577c1656', 'user', '0', '用户管理', 'User Manage', 1, 0, NULL, 'HomeFilled', 1, 2, '2022-04-29 15:18:17', '2022-04-29 15:18:20', NULL);
+INSERT INTO `sys_menu` VALUES ('09a724dfb41f47e50cb88ecd577c1657', 'role', '0', '权限管理', 'Role Manage', 1, 0, NULL, 'HomeFilled', 1, 2, '2022-04-29 15:18:17', '2022-04-29 15:18:20', NULL);
+
+-- ----------------------------
+-- Table structure for sys_menu_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu_role`;
+CREATE TABLE `sys_menu_role`  (
+  `role_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+  `menu_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单id',
+  PRIMARY KEY (`role_id`, `menu_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '菜单角色' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_menu_role
+-- ----------------------------
+INSERT INTO `sys_menu_role` VALUES ('09a724dfb41f47e50cb88ecd577c1666', '09a724dfb41f47e50cb88ecd577c1655');
+INSERT INTO `sys_menu_role` VALUES ('09a724dfb41f47e50cb88ecd577c1666', '09a724dfb41f47e50cb88ecd577c1656');
+INSERT INTO `sys_menu_role` VALUES ('09a724dfb41f47e50cb88ecd577c1777', '09a724dfb41f47e50cb88ecd577c1655');
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名称',
+  `status` int(1) NOT NULL COMMENT '状态：0、正常，1、禁用',
+  `creator` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('09a724dfb41f47e50cb88ecd577c1666', '管理员权限', 0, NULL, '2022-04-29 15:19:51', '2022-04-29 15:19:54', NULL);
+INSERT INTO `sys_role` VALUES ('09a724dfb41f47e50cb88ecd577c1777', '普通用户权限', 0, NULL, '2022-04-29 15:19:51', '2022-04-29 15:19:54', NULL);
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -109,19 +168,41 @@ CREATE TABLE `sys_user`  (
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名称',
   `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户昵称',
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户密码',
-  `status` int(1) NULL DEFAULT NULL COMMENT '状态：0：删除，1：正常，2：禁用',
+  `status` int(1) NULL DEFAULT NULL COMMENT '状态：0：正常，1：删除，2：禁用',
   `type` int(1) NULL DEFAULT NULL COMMENT '类型：0、普通用户，1、管理员',
   `open_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '微信唯一标识',
-  `birthday` date NULL DEFAULT NULL COMMENT '生日',
+  `birthday` date NULL DEFAULT NULL COMMENT '出生日期',
   `sex` int(1) NULL DEFAULT NULL COMMENT '性别：0：男；1：女',
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话',
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
-  `classify` int(1) NULL DEFAULT NULL COMMENT '租户分类：字典值',
+  `id_card` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份证号',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('bc3ac26e69731b617eb80274453f6db1', 'test', '管理员', '$2a$10$tlWWfjTObqLsC6ONrhNL/.GIpAoFu205TXPK6xUPHHr1kA/paK4lq', 0, 0, NULL, '2022-02-14', 0, 'test@qq.com', '18893817562', '兰州安宁区永新华世界湾', '', '2022-04-29 15:22:18', '2022-04-29 15:22:20', '');
+INSERT INTO `sys_user` VALUES ('bc3ac26e69731b617eb80274453f6dbb', 'admin', '管理员', '$2a$10$tlWWfjTObqLsC6ONrhNL/.GIpAoFu205TXPK6xUPHHr1kA/paK4lq', 0, 1, NULL, '2022-02-14', 0, 'admin@qq.com', '18893817562', '兰州安宁区永新华世界湾', '', '2022-04-29 15:22:18', '2022-04-29 15:22:20', '');
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`  (
+  `user_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
+  `role_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色id',
+  PRIMARY KEY (`user_id`, `role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户角色' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('bc3ac26e69731b617eb80274453f6db1', '09a724dfb41f47e50cb88ecd577c1777');
+INSERT INTO `sys_user_role` VALUES ('bc3ac26e69731b617eb80274453f6dbb', '09a724dfb41f47e50cb88ecd577c1666');
 
 SET FOREIGN_KEY_CHECKS = 1;
